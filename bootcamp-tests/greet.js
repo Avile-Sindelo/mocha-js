a5a32bee327695d9513fcb212aa79966329fc57d
Avile Sindelo <avilesindelo@gmail.com>
2023-04-13 15:23:31 +0200

    Adding the greet function, from the correct branch

--- bootcamp-tests/greet.js
+++ bootcamp-tests/greet.js
@@ -1,4 +1,13 @@
 function greet(neighbor){
+	//Make sure the name of the neighbor is a string
+	if(typeof neighbor != 'string'){
+		return 'This function expects names of people to be strings of text'
+	}
+
+	//Make sure the parameter is not empty
+	if(neighbor == ' ' || neighbor == ''){
+		return 'Please pass a name to the function'
+	}
 	return 'Hello '+neighbor;
 }
 
--- bootcamp-tests/test/greet.test.js
+++ bootcamp-tests/test/greet.test.js
@@ -1,6 +1,14 @@
 describe('Greet' , function(){
-    it('should test the greet function of the past bootcamp' , function(){
+    it('should test the properly greets the neighbor' , function(){
         assert.equal(greet('Sibulele'), 'Hello Sibulele');
+    });
+
+    it('should test what the function does if the neighbor/s name is not a string', function(){
+        assert.equal(greet(9), 'This function expects names of people to be strings of text');
+    });
+
+    it('should test what happens when the function is is given no name to greet', function(){
+        assert.equal(greet(' '), 'Please pass a name to the function');
     });
 
 });
